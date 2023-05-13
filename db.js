2201f90de4b8a8ef27fdf77c6709a592b644f12f
unknown <yjs12180825@gmail.com>
2023-05-13 22:18:45 +0900

    db connect

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
-mongoose.connect("svc.sel4.cloudtype.app:31481", {
+mongoose.connect("mongodb+srv://svc.sel4.cloudtype.app:31481", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 }).then(() => {

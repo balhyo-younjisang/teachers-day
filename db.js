ac181d94cfa3fdeb4f2742d8b0f0412cee45111b
unknown <yjs12180825@gmail.com>
2023-05-14 21:36:32 +0900

    db

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 // dev db server uri = mongodb://localhost:27017/Teachersday
 //deploy db server uri = mongodb://admin:Yydo0825..@mongo:27017
-mongoose.connect("mongodb://admin:Yydo0825..@mongo:27017", {
+mongoose.connect(process.env.DB_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 }).then(() => {
